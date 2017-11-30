553dd6eed7406fd021acaf7c5f91c35e0d141574
sguthals <sarah@guthals.com>
2017-11-29 20:39:33 -0800

    moved customBlocks out of blockly directory

---
+++ customBlocks.js
@@ -0,0 +1,59 @@
+
+goog.require('Blockly.Blocks');
+
+/************************
+ *  BLOCKLY DEFINITIONS  *
+ *************************/
+
+Blockly.Blocks['whenrunclicked'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("When RUN is clicked");
+        this.setNextStatement(true, null);
+        this.setColour(65);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['clearscreen'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("Clear Canvas");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(175);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['catpose'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("Change Pose")
+            .appendField(new Blockly.FieldDropdown([["1","1"], ["2","2"], ["3","3"]]), "catIndex");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(175);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.JavaScript['whenrunclicked'] = function(block) {
+    var statements_name = Blockly.JavaScript.statementToCode(block, 'NAME');
+    var blockCode = statements_name;
+    return blockCode;
+};
+
+Blockly.JavaScript['clearscreen'] = function(block) {
+    var blockCode = 'clearCanvas();';
+    return blockCode;
+};
+
+Blockly.JavaScript['catpose'] = function(block) {
+    var dropdown_catindex = block.getFieldValue('catIndex');
+    var blockCode = 'updatePose('+dropdown_catindex+');';
+    return blockCode;
+};
